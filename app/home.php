beea7ea7b28ea50da7f01cdf7b5b5b52757bb9a8
Sandeep Kumar <ki.sandeep11@gmail.com>
2020-03-10 21:12:47 +0530

    ...

--- app/home.php
+++ app/home.php
@@ -3,30 +3,30 @@ use Illuminate\Database\Capsule\Manager as Database;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Container\Container;
 
-$db = new Database;
+// $db = new Database;
 
-$db->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'cip',
-    'username'  => 'root',
-    'password'  => '',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-]);
-// Set the event dispatcher used by Eloquent models... (optional)
-$db->setEventDispatcher(new Dispatcher(new Container));
+// $db->addConnection([
+//     'driver'    => 'mysql',
+//     'host'      => 'localhost',
+//     'database'  => 'cip',
+//     'username'  => 'root',
+//     'password'  => '',
+//     'charset'   => 'utf8',
+//     'collation' => 'utf8_unicode_ci',
+//     'prefix'    => '',
+// ]);
+// // Set the event dispatcher used by Eloquent models... (optional)
+// $db->setEventDispatcher(new Dispatcher(new Container));
 
-// Make this Capsule instance available globally via static methods... (optional)
-$db->setAsGlobal();
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$db->bootEloquent();
-$rows = Database::table('branch')->limit(100)->get();
-echo '<pre>';
-foreach($rows as $row){
-    print_r($row);
-}
+// // Make this Capsule instance available globally via static methods... (optional)
+// $db->setAsGlobal();
+// // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+// $db->bootEloquent();
+// $rows = Database::table('branch')->limit(100)->get();
+// echo '<pre>';
+// foreach($rows as $row){
+//     print_r($row);
+// }
 
 $data = ['title' => 'home'];
 view('welcome', $data);
